e14ad629a9ed5c584902566aff02c292350d8b53
Zeindi <zadakhadzhiev@gmail.com>
2023-05-05 02:35:47 +0600

    strange name output

--- main.go
+++ main.go
@@ -1,98 +1,120 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io/ioutil"
 	"math/rand"
+	"os"
 	"strings"
 	"time"
 )
 
-func main() {
-	// read data from file
-	data, err := ioutil.ReadFile("names.txt")
+func readData(fileName string) ([]string, error) {
+	file, err := os.Open(fileName)
 	if err != nil {
-		panic(err)
+		return nil, err
+	}
+	defer file.Close()
+
+	var data []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		data = append(data, scanner.Text())
 	}
 
-	// convert data to lowercase string
-	text := strings.ToLower(string(data))
+	return data, nil
+}
 
-	// calculate bigram probabilities
-	bigrams := make(map[string]int)
-	for i := 0; i < len(text)-1; i++ {
-		bigram := text[i : i+2]
-		bigrams[bigram]++
+func calculateProbabilities(data []string) map[string]float64 {
+	bigramCounts := make(map[string]int)
+	totalCount := 0
+
+	for _, name := range data {
+		name = "^" + name + "$" // add "^" and "$" markers to indicate the beginning and end of the name
+		for i := 0; i < len(name)-1; i++ {
+			bigram := name[i : i+2]
+			bigramCounts[bigram]++
+			totalCount++
+		}
 	}
 
-	// calculate total number of bigrams
-	total := 0
-	for _, count := range bigrams {
-		total += count
-	}
-
-	// calculate probabilities
 	probabilities := make(map[string]float64)
-	for bigram, count := range bigrams {
-		probabilities[bigram] = float64(count) / float64(total)
+	for bigram, count := range bigramCounts {
+		probabilities[bigram] = float64(count) / float64(totalCount)
 	}
 
-	// print table of bigram probabilities
-	fmt.Println("Bigram probabilities:")
+	return probabilities
+}
+
+func selectFirstLetter(probabilities map[string]float64) string {
+	rand.Seed(time.Now().UnixNano()) // initialize the random number generator
+	sum := 0.0
+	threshold := rand.Float64()
+
 	for bigram, probability := range probabilities {
-		fmt.Printf("%s: %.4f\n", bigram, probability)
+		if strings.HasPrefix(bigram, "^") {
+			sum += probability
+			if sum > threshold {
+				return string(bigram[1])
+			}
+		}
 	}
 
-	// generate a name
-	rand.Seed(time.Now().UnixNano())
-	name := ""
-	lastLetter := "^"
+	return "" // should never happen
+}
+
+func generateName(probabilities map[string]float64) string {
+	name := selectFirstLetter(probabilities)
 	for {
-		// get possible next bigrams
-		var candidates []string
-		for bigram := range probabilities {
-			if strings.HasPrefix(bigram, lastLetter) {
-				candidates = append(candidates, bigram)
-			}
+		var lastBigram string
+		if len(name) > 1 {
+			lastBigram = name[len(name)-2:] + "$"
+		} else {
+			lastBigram = "^" + name + "$"
 		}
-
-		// break out of loop if there are no candidates
-		if len(candidates) == 0 {
-			break
-		}
-
-		// choose next bigram randomly based on probabilities
-		sum := 0.0
-		for _, bigram := range candidates {
-			sum += probabilities[bigram]
-		}
-		randVal := rand.Float64() * sum
-		nextBigram := ""
-		for _, bigram := range candidates {
-			randVal -= probabilities[bigram]
-			if randVal <= 0 {
-				nextBigram = bigram
-				break
-			}
-		}
-
-		// add next letter to name
-		nextLetter := string(nextBigram[1])
-		if nextLetter == "$" {
+		nextLetter := selectNextLetter(lastBigram, probabilities)
+		if nextLetter == "" {
 			break
 		}
 		name += nextLetter
-		lastLetter = nextLetter
+	}
+	return strings.Trim(name, "^$")
+}
+
+func selectNextLetter(lastBigram string, probabilities map[string]float64) string {
+	rand.Seed(time.Now().UnixNano())
+	sum := 0.0
+	threshold := rand.Float64()
+
+	for bigram, probability := range probabilities {
+		if strings.HasPrefix(bigram, lastBigram) {
+			sum += probability
+			if sum > threshold {
+				return string(bigram[1])
+			}
+		}
 	}
 
-	// capitalize first letter of name
-	if len(name) > 0 {
-		name = strings.ToUpper(name[:1]) + name[1:]
+	return "" // should never happen
+}
+
+func printProbabilities(probabilities map[string]float64) {
+	fmt.Printf("%-3s %-7s\n", "Bigram", "Probability")
+	for bigram, probability := range probabilities {
+		fmt.Printf("%-3s %-7.5f\n", bigram, probability)
+	}
+}
+
+func main() {
+	data, err := readData("names.txt")
+	if err != nil {
+		fmt.Println("Error reading data:", err)
+		return
 	}
 
-	if name != "" {
-		fmt.Println("Generated name:", name)
-	} else {
-		fmt.Println("Unable to generate name")
-	}
+	probabilities := calculateProbabilities(data)
+	printProbabilities(probabilities)
+
+	name := generateName(probabilities)
+	fmt.Println("Generated name:", name)
 }
